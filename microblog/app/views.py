6b784a060332fb30e87b214fb8e637a32324023f
letfly <letfly@outlook.com>
2015-05-27 15:05:36 +0800

    二：templates

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,21 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	user = {'nickname': 'letfly'} # fake user
+	posts = [ # fake array of posts
+		{
+			'author': {'nickname': 'John'},
+			'body': 'Beautiful day in Portland!'
+		},
+		{
+			'author': {'nickname': 'Susan'},
+			'body': 'The Avengers movie was so cool!'
+		}
+	]
+	return render_template('index.html',
+							title='Home',
+							user=user,
+							posts=posts)
