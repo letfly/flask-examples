511fb716608c5cf459d0305d9f2a32bf0bf3b456
letfly <letfly@outlook.com>
2015-05-28 13:38:51 +0800

    三：login form

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
-
+from flask import render_template, flash, redirect
+from .forms import LoginForm
+'''
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,3 +20,15 @@ def index():
 							title='Home',
 							user=user,
 							posts=posts)
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+				(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',
+							title='Sign In',
+							form=form,
+							providers=app.config['OPENID_PROVIDERS'])
