2c817946c0e7f54e5557c4db452918b8b2e9548e
letfly <letfly@outlook.com>
2015-06-01 00:55:53 +0800

    五：User Logins

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+lm.login_view = 'login'
 
 from app import views, models
--- microblog/app/models.py
+++ microblog/app/models.py
@@ -10,6 +10,18 @@ class User(db.Model):
     #role = db.Column(db.SmallInteger, default = ROLE_USER)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
 
+    def is_authenticated(self):
+	return True
+    def is_active(self):
+	return True
+    def is_anonymous(self):
+	return False
+    def get_id(self):
+	try:
+	    return unicode(self.id)  # python 2
+	except NameError:
+	    return str(self.id)  # python 3
+    
     def __repr__(self):
 	return '<User %r>' % (self.nickname)
 
