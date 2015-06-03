4b0701ba5e8d09830f10b27db885817a872678d8
letfly <letfly@outlook.com>
2015-06-03 15:07:59 +0800

    myproject 四：opetate database. A:amend myproject/app/models.py myproject/app/views.py; B:python db_mirgate.py

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -12,3 +12,11 @@ class User(db.Model):
     posts = db.relationship('Post', backref = 'author', lazy='dynamic')
     def __repr__(self):
 	return '<User %r>' % self.nickname
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+	return '<Post %r>' % self.body
--- myproject/app/views.py
+++ myproject/app/views.py
@@ -1,5 +1,34 @@
 from app import app
-
+'''
 @app.route('/')
 def index():
     return 'hello world, hello flask'
+'''
+from flask import render_template, flash, redirect, session, url_for, request, g
+from app import db
+from models import User, Post, ROLE_USER, ROLE_ADMIN
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/adduser/<nickname>/<email>')
+def adduser(nickname, email):
+    u = User(nickname=nickname, email=email)
+    try:
+	db.session.add(u)
+	db.session.commit()
+	return 'add successful'
+    except Exception, e:
+	return 'something go wrong'
+@app.route('/getuser/<nickname>')
+def getuser(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+    return render_template('user.html', user=user)
+@app.errorhandler(404)
+def internal_error(error):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
