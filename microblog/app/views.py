23311ce8e9015b8f11ecbc82db3cc9e181e3c751
letfly <letfly@outlook.com>
2015-07-26 23:29:55 +0800

    六：Profile Page And Avatars[更多相关个人信息]

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, \
     login_required
+from datetime import datetime
 from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
@@ -14,6 +15,10 @@ def load_user(id):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
--- microblog/db_migrate.py
+++ microblog/db_migrate.py
@@ -1,4 +1,4 @@
-#!flask/bin/python
+#!microblog_env/bin/python
 import imp
 from migrate.versioning import api
 from app import db
